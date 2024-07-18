b3c94cfcfbf75e5222eef0f12f3753d35616e9d4
Aleksey Demchenko <ad@pesochin.net>
2024-07-18 20:02:03 +0300

    RA: 1. Webpack: Decomposition of the configuration. DevServer lesso_03 Fix001

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import { testFunction } from './test';
 
 testFunction(42);
 console.log(testFunction(42));
+
+document.body.innerHTML = `<div>${testFunction(42)}</div>`;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,25 @@
 import path from 'path';
 import webpack from 'webpack';
 import { distWebpackConfig } from './config/dist/distWebpackConfig';
-import { DistPath } from './config/dist/types/config';
+import { DistEnv, DistPath } from './config/dist/types/config';
 
-const paths: DistPath = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  dist: path.resolve(__dirname, 'dist'),
-  html: path.resolve(__dirname, 'public', 'index.html'),
+export default (env: DistEnv) => {
+  const paths: DistPath = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    dist: path.resolve(__dirname, 'dist'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+  };
+
+  const mode = env.mode || 'development';
+  const PORT = env.port || 3000;
+
+  const isDev = mode === 'development';
+
+  const config: webpack.Configuration = distWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+  return config;
 };
-
-const mode = 'development';
-const isDev = mode === 'development';
-const PORT = 3000;
-
-const config: webpack.Configuration = distWebpackConfig({
-  mode: 'development',
-  paths,
-  isDev,
-  port: PORT,
-});
-
-export default config;

819f910d1fdcb4735505acde1545765e0a76c5f1
Aleksey Demchenko <ad@pesochin.net>
2024-08-19 01:51:00 +0300

    RA: 3. CI pipeline: lesson_27 add003

--- src/app/providers/ThemeProvider/index.tsx
+++ src/app/providers/ThemeProvider/index.tsx
@@ -1,6 +1,6 @@
 import ThemeProvauder from './ui/ThemeProvauder';
 import { useTheme } from './lib/useTheme';
 import { Theme } from './lib/ThemeContext';
-import React from 'react';
+// import React from 'react';
 
 export { ThemeProvauder, useTheme, Theme };
--- src/shared/consfig/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/consfig/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -3,10 +3,16 @@ import { StoryFn } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 import React from 'react';
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: StoryFn) => {
-  return (
-    <div className={`app ${theme}`}>
-      <StoryComponent />
-    </div>
-  );
+export const ThemeDecorator = (theme: Theme): ((StoryComponent: StoryFn) => JSX.Element) => {
+  const ThemedComponent = (StoryComponent: StoryFn): JSX.Element => {
+    return (
+      <div className={`app ${theme}`}>
+        <StoryComponent />
+      </div>
+    );
+  };
+
+  ThemedComponent.displayName = `ThemeDecorator(${theme})`;
+
+  return ThemedComponent;
 };

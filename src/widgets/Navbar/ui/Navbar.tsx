2edbfa4b7c27cac25e40a3620289e8b159d40965
Aleksey Demchenko <ad@pesochin.net>
2024-08-26 15:22:21 +0300

    RA: 3.Update Sidebar: lesson_28

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import classes from './Navbar.module.scss';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps): JSX.Element => {
-  const { t } = useTranslation();
-
   return (
     <div className={classNames(classes.navbar, {}, [className])}>
       <div className={classes.links}>/</div>
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -5,8 +5,8 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
 import { Button, SizeButton, ThemeButton } from 'shared/ui/Button/Button';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { t } from 'i18next';
 import { RoutePath } from 'shared/consfig/routeConfig/routeConfig';
+import { useTranslation } from 'react-i18next';
 
 import MaineIcone from 'shared/assets/icons/maine.svg';
 import AboutIcone from 'shared/assets/icons/about.svg';
@@ -17,6 +17,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps): JSX.Element => {
   const [collapsed, setCollapsed] = useState(false);
+  const { t } = useTranslation();
 
   const handleToggle = (): void => {
     setCollapsed((prev) => !prev);

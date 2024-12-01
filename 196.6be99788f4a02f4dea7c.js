b6e856600dd12fb3587f4ec3b553025332c54682
Shimodos <109157354+Shimodos@users.noreply.github.com>
2024-12-01 18:55:38 +0000

    Deploying to gh-pages from @ Shimodos/ReactAdvanced@dd0090a0d4d323c9b5e2ed3bb2e4c3ea0f2a951e 🚀

--- 196.6be99788f4a02f4dea7c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproduction_project=self.webpackChunkproduction_project||[]).push([[196],{4196:(e,r,t)=>{t.r(r),t.d(r,{default:()=>o});var c=t(5893),n=t(8418),s=t(2327),u=t(5182);const o=function(){var e=(0,s.$)().t;return(0,c.jsxs)(u.T,{children:[e("MainPage"),(0,c.jsx)(n.A,{})]})}}}]);
---
+++ 986.31b3e3cc45e9aed21106.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkproduction_project=self.webpackChunkproduction_project||[]).push([[986],{2986:(t,e,r)=>{r.r(e),r.d(e,{default:()=>n});var u=r(5893),c=r(2327),o=r(8798);const n=function(){var t=(0,c.$)("about").t;return(0,u.jsx)(o.T,{children:t("AboutPage")})}}}]);

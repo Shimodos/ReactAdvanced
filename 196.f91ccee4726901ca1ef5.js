45223e575f169d303eef74148ad0f048612b38e5
Shimodos <109157354+Shimodos@users.noreply.github.com>
2025-01-04 16:35:41 +0000

    Deploying to gh-pages from @ Shimodos/ReactAdvanced@e8e672eb25e4882e753874c495464e3fec74f6a8 🚀

---
+++ 196.f91ccee4726901ca1ef5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkproduction_project=self.webpackChunkproduction_project||[]).push([[196],{4196:(e,r,t)=>{t.r(r),t.d(r,{default:()=>o});var c=t(5893),n=t(8418),s=t(4478),u=t(8798);const o=function(){var e=(0,s.$)().t;return(0,c.jsxs)(u.T,{children:[e("MainPage"),(0,c.jsx)(n.A,{})]})}}}]);

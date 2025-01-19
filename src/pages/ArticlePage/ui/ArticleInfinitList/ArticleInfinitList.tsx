bad6909ee851a38005b62d6d2541d5836bf45e72
Aleksey Demchenko <ad@pesochin.net>
2025-01-19 19:28:03 +0200

    RA: 11. RTK query. The beginning of a major refactoring, lesson_70 Part004

---
+++ src/pages/ArticlePage/ui/ArticleInfinitList/ArticleInfinitList.tsx
@@ -0,0 +1,32 @@
+import { ArticleList } from 'entities/Article';
+import {
+  getArticlePageError,
+  getArticlePageIsLoading,
+  getArticlePageView
+} from '../../model/selectors/articlePageSelectors';
+import { getArticle } from '../../model/slices/ArticlePageSlice';
+import { useSelector } from 'react-redux';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Text, TextSize } from 'shared/ui/Text/Text';
+
+interface ArticleInfinitListProps {
+  className?: string;
+}
+export const ArticleInfinitList = ({ className }: ArticleInfinitListProps) => {
+  const { t } = useTranslation('article');
+  const articles = useSelector(getArticle.selectAll);
+  const isLoading = useSelector(getArticlePageIsLoading);
+  const view = useSelector(getArticlePageView);
+  const error = useSelector(getArticlePageError);
+
+  if (error) {
+    return <Text size={TextSize.M} text={t('Error')} />;
+  }
+
+  return (
+    <div className={classNames('', {}, [className])}>
+      <ArticleList isLoading={isLoading} view={view} articcle={articles} />
+    </div>
+  );
+};
--- src/pages/ArticlePage/ui/ArticlePage.tsx
+++ src/pages/ArticlePage/ui/ArticlePage.tsx
@@ -1,26 +1,21 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import classes from './ArticlePage.module.scss';
 import { useTranslation } from 'react-i18next';
-import { ArticleList } from 'entities/Article/ui/ArticleList/ArticleList';
 import {
   DynamicModuleLoder,
   ReducersList
 } from 'shared/lib/components/DynamicModuleLoder/DynamicModuleLoder';
-import { articlePageReducer, getArticle } from '../model/slices/ArticlePageSlice';
+import { articlePageReducer } from '../model/slices/ArticlePageSlice';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { useSelector } from 'react-redux';
-import {
-  // getArticlePageError,
-  getArticlePageIsLoading,
-  getArticlePageView
-} from '../model/selectors/articlePageSelectors';
+
 import { useCallback } from 'react';
 import { Page } from 'shared/ui/Page/Page';
 import { fetchNextArticlePage } from '../model/services/fetchNextArticlePage/fetchNextArticlePage';
 import { initArticlesPage } from '../model/services/initArticlesPage/initArticlesPage';
 import { ArticlePageFilters } from './ArticlePageFilters/ArticlePageFilters';
 import { useSearchParams } from 'react-router-dom';
+import { ArticleInfinitList } from './ArticleInfinitList/ArticleInfinitList';
 
 interface ArticleDetailsPageProps {
   className?: string;
@@ -33,9 +28,6 @@ const reducers: ReducersList = {
 const ArticlePage = ({ className }: ArticleDetailsPageProps) => {
   const { t } = useTranslation('article');
   const dispatch = useAppDispatch();
-  const articles = useSelector(getArticle.selectAll);
-  const isLoading = useSelector(getArticlePageIsLoading);
-  const view = useSelector(getArticlePageView);
   const [searchParams] = useSearchParams();
 
   const onLoadingMore = useCallback(() => {
@@ -53,7 +45,7 @@ const ArticlePage = ({ className }: ArticleDetailsPageProps) => {
         className={classNames(classes.ArticlePage, {}, [className])}
       >
         <ArticlePageFilters />
-        <ArticleList isLoading={isLoading} view={view} articcle={articles} />
+        <ArticleInfinitList />
       </Page>
     </DynamicModuleLoder>
   );
